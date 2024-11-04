79b8ac7ec7a307f49c5f558958e348d3d2c48d72
YaqubRahman <YaqubRahman999@gmail.com>
2024-11-04 18:04:10 +0000

    learnt about api stuff and added a counter

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
+<script setup>
+const sayHello = () => console.log("Hello")
+const counter = useCounter();
+
+const response = await $fetch('/api/hello')
+console.log(response)
+</script>
+
 <template>
     <div class="Main" >
+        <div id="main">
         <Alert />
         <Profile />
         <img src="/1.png" alt="" />
+
+        Counter: {{ counter }}
+        <button @click="counter++">
+            +
+        </button>
+        <button @click="counter--">
+            -
+        </button>
+        <Counter id="counter"/>
+        </div>
     </div>
 </template>
